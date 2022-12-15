529d7df2d65e22060da301ef16681941519f1527
reut <reuttmm@gmail.com>
2022-12-15 17:58:22 +0200

    remove add rectangle.

--- main.py
+++ main.py
@@ -23,14 +23,14 @@ def emotion(img):
 while True:
     success, img = cap.read()
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(imgGray, 1.1, 4)
+    faces = faceCascade.detectMultiScale(imgGray, 2, 4)
     for (x, y, w, h) in faces:
         area = w*h
         if area > minArea:
-            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            #cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
             cv2.putText(img, emotion(img[y:y+h, x:x+w]), (x,y-5),cv2.FONT_HERSHEY_COMPLEX_SMALL,1, color, 2)
-            imgRoi = img[y:y+h, x:x+w]
-            cv2.imshow("ROI", imgRoi)
+            #imgRoi = img[y:y+h, x:x+w]
+            #cv2.imshow("ROI", imgRoi)
 
     cv2.imshow("Result", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
